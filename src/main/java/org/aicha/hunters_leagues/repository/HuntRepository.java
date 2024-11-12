9b90bbc04ebc9bf40f170c34e3231360fc41629a
ettabet aicha <aicha70tabite@gmail.com>
2024-11-12 09:35:18 +0100

    add hunt logic

--- src/main/java/org/aicha/hunters_leagues/repository/HuntRepository.java
+++ src/main/java/org/aicha/hunters_leagues/repository/HuntRepository.java
@@ -2,8 +2,18 @@ package org.aicha.hunters_leagues.repository;
 
 import org.aicha.hunters_leagues.domain.Hunt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface HuntRepository extends JpaRepository<Hunt, UUID> {
+    List<Hunt> findBySpeciesId(UUID speciesId);
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM hunt WHERE species_id = :speciesId", nativeQuery = true)
+    void deleteBySpeciesId(@Param("speciesId") UUID speciesId);
 }
